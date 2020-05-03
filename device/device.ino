d66b4302e704c332789b2c49e1af480a530cd0fc
Diana <mdiannna@yahoo.com>
2020-05-03 13:08:09 +0300

    Sequential system working on Arduino with one task

--- device/device.ino
+++ device/device.ino
@@ -1,32 +1,33 @@
-#include "os.h"
+ #include "os.h"
+#include "timer-api.h"
 
 OS_TaskDescriptor_t task1;
 
+
+void timer_handle_interrupts(int timer) {
+	OS_Scheduler();
+}
 
 void SerialInit() {
 	Serial.begin(9600);
 }
 
-void PrintHello() {
+int PrintHelloTaskRun() {
 	Serial.println("Hello!");
+  return 0;
 }
+
+
 
 void setup() {
 	SerialInit();
 
+  task1 = OS_TaskDescriptorDefault();
 	task1.recurrence = 1000; //1 second
-	task1.recurrenceCnt = 500;
-	task1.startupTimeout = 1000;  // 1 second
-	task1.timeout = 1000;
-	task1.priorty = 1;
 
-	// int32_t (*task) ();
-	task1.task = PrintHello;
-	task1.task_state = ENABLE;
+	OS_TaskInit( PrintHelloTaskRun, &task1 );
 
-
-	OS_TaskInit( PrintHello, &task1 );
-
+  timer_init_ISR_1KHz(TIMER_DEFAULT);
 }
 
 void loop() {
